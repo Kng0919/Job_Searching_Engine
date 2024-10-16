8af5d42af824328c3343e6a9b731106a263c7dbe
kng0919 <kng0919@terpmail.umd.edu>
2024-10-16 02:00:51 -0400

    Implemented the functionality of page directing to New User and Get Started buttons

--- app.py
+++ app.py
@@ -7,5 +7,14 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+#Register Page
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'GET':
+        return render_template("register.html")
+    else:
+        #POST method - To be implemented
+        pass
+
 if __name__ == "__main__":
     app.run()
